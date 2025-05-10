084b9f0f21ef905d24659bf553626529df42d8e6
OlgaHu <olga.hu.libra@gmail.com>
2025-05-10 15:11:35 +0800

    [AI 聊天]: websocket 串接

---
+++ src/main/java/com/olga/ai/test1/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.olga.ai.test1.config;
+
+import com.olga.ai.test1.handler.AiChatWebSocketHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private AiChatWebSocketHandler aiChatWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(aiChatWebSocketHandler, "/ws/chat").setAllowedOrigins("*");
+    }
+}
